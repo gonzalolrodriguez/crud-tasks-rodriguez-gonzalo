0c6f587764e2ee78b7161cf19c14a41deb545717
Gonzalo <gonzalo.rodriguez677@gmail.com>
2025-08-10 23:42:22 -0300

    Se configuró app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./src/config/database.js";
+import routerTask from "./src/routes/task.routes.js";
+import routerUser from "./src/routes/user.routes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api", routerTask);
+app.use("/api", routerUser);
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(
+            `Conexión con la base de datos establecida \nhttp://localhost:${PORT}/api/task \nhttp://localhost:${PORT}/api/user`
+        );
+    });
+});
